e731ae1ce1397c9160ee079b436ba38ae18eb4b9
user <user@WIN-RA2CDU1E29O>
2021-09-27 23:27:59 +0300

    Добавьте файлы проекта.

---
+++ INotifyPrppertyChanget_TaskDex/Car.cs
@@ -0,0 +1,73 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace INotifyPrppertyChanget_TaskDex
+{
+    class Car : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private readonly string _brand;
+        private readonly string _model;
+        private readonly string _win;
+        private string _namber;
+        private string _carOwner;
+
+        public Car(string breand, string model, string win)
+        {
+            _brand = breand;
+            _model = model;
+            _win = win;
+        }
+
+        private void CangedProperty([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public override string ToString()
+        {
+            return "\nМврка: " + Brand + "\nМодель: " + Model + "\nГос Номер: " + Namber + "\nВладелец: " + CarOwner;
+        }
+
+        public string Brand
+        {
+            get => _brand;
+        }
+
+        public string Model
+        {
+            get => _model;
+        }
+
+        public string Win
+        {
+            get => _win;
+        }
+
+        public string Namber
+        {
+            get => _namber;
+            set
+            {
+                if (value != _namber)
+                {
+                    _namber = value;
+                    CangedProperty();
+                }
+            }
+        }
+
+        public string CarOwner
+        {
+            get => _carOwner;
+            set 
+            {
+                if (value != _carOwner)
+                {
+                    _carOwner = value;
+                    CangedProperty();
+                }
+            }
+        }
+    }
+}
---
+++ INotifyPrppertyChanget_TaskDex/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace INotifyPrppertyChanget_TaskDex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car person = new Car("BMW", "335D", "BWRS1245TT12T56")
+            {
+                Namber = "T 335 BH", 
+                CarOwner = "Петров Александр Васильевич" 
+            };
+
+            person.PropertyChanged += CarPropertyChanged;
+            person.CarOwner = "Иванов Сергей Аркадьевич";
+            person.Namber = "C 513 AB";
+        }
+
+        private static void CarPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            switch (e.PropertyName)
+            {
+                case "Namber":
+                    Console.WriteLine($"Данные о номере авто изменены: {e.PropertyName} {sender}\n");
+                    break;
+                case "CarOwner":
+                    Console.WriteLine($"Данные о владельце авто изменены: {e.PropertyName} {sender}\n");
+                    break;
+            }
+        }
+    }
+}
